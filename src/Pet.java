66b96e44002731d33f88c7bea06ac5ae29f19f35
user <armiyanova.di@gmail,com>
2025-12-17 19:06:02 +0500

    1

--- src/Pet.java
+++ src/Pet.java
@@ -3,13 +3,15 @@ public class Pet {
     private String name;
     private int age;
     private String kind;
+    private String color;
     private boolean adopted;
 
     // Constructor
-    public Pet(String name, int age, String kind) {
+    public Pet(String name, int age, String kind , String color) {
         this.name = name;
         this.age = age;
         this.kind = kind;
+        this.color = color;
         this.adopted = false;
     }
 
@@ -29,10 +31,17 @@ public class Pet {
     public boolean getAdopted() {
         return adopted;
     }
+    public String getColor() {
+        return color;
+    }
+
 
     // Setter
     public void setAdopted(boolean adopted) {
         this.adopted = adopted;
+    }
+    public void setColor(String color) {
+        this.color = color;
     }
 
     // Method
@@ -41,6 +50,7 @@ public class Pet {
         System.out.println("Kind: " + kind);
         System.out.println("Age: " + age);
         System.out.println("Adopted: " + adopted);
+        System.out.println("Color: " + color);
         System.out.println();
     }
 }
